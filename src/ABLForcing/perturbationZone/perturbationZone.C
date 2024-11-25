6475acc6591ac27be442c66ef6a50d57019f6c00
Matt Churchfield <mchurchf@kl3.head.cm.kestrel.hpc.nrel.gov>
2024-11-25 11:42:38 -0700

    Small update to allow more control of slab local perturbation update if inflow has great wind speed variation, as with hurrican simulation.

--- src/ABLForcing/perturbationZone/perturbationZone.C
+++ src/ABLForcing/perturbationZone/perturbationZone.C
@@ -74,6 +74,7 @@ void Foam::perturbationZone<Type>::initialize()
     updateMode_.setSize(nZones_);
     updatePeriod_.setSize(nZones_);
     updatePeriodSlab_.setSize(nZones_);
+    updatePeriodScalar_.setSize(nZones_);
     PBLHeight_.setSize(nZones_);
     applicationMode_.setSize(nZones_);
     clipAtTwoThirdsPBLHeight_.setSize(nZones_);
@@ -133,6 +134,8 @@ void Foam::perturbationZone<Type>::readSubDict()
         EckertNumber_[m] = subSubDict.lookupOrDefault<scalar>("EckertNumber",0.2);
 
         updateMode_[m] = subSubDict.lookupOrDefault<word>("updateMode","fixedFrequency");
+
+	updatePeriodScalar_[m] = subSubDict.lookupOrDefault<scalar>("updatePeriodScalar",1.0);
 
         if (subSubDict.found("PBLHeight"))
         {
@@ -967,7 +970,7 @@ void Foam::perturbationZone<Type>::update()
                     vector n = d/mag_d;
 
                     Info << "updatePeriod = " << updatePeriod_[m] << endl;
-                    updatePeriod_[m] = mag_d/(max(mag(vel & n),1.0E-6)*sign(vel & n));
+                    updatePeriod_[m] = updatePeriodScalar_[m] * (mag_d/(max(mag(vel & n),1.0E-6)*sign(vel & n)));
 
                     Info << "vel = " << vel << tab << "d = " << d << tab << "mag(d) = " << mag_d << tab << "(vel & n) = " << (vel & n) << endl;
                     Info << "updatePeriod = " << updatePeriod_[m] << endl;
@@ -1031,7 +1034,7 @@ void Foam::perturbationZone<Type>::update()
                     vector vel = velAvg[k];
 
                     Info << "updatePeriod = " << updatePeriodSlab_[m][k] << endl;
-                    updatePeriodSlab_[m][k] = mag_d/(max(mag(vel & n),1.0E-6)*sign(vel & n));
+                    updatePeriodSlab_[m][k] = updatePeriodScalar_[m] * (mag_d/(max(mag(vel & n),1.0E-6)*sign(vel & n)));
 
                     Info << "vel = " << vel << tab << "d = " << d << tab << "mag(d) = " << mag_d << tab << "(vel & n) = " << (vel & n) << endl;
                     Info << "updatePeriod = " << updatePeriodSlab_[m][k] << endl;       
